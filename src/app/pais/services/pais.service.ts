8609b0a9241355d138240be8355c51ddad76dcce
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-09-23 20:02:38 -0300

    Cambio y creacion apikey

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -8,13 +8,13 @@ import { Country } from '../interfaces/pais-interface';
 })
 export class PaisService {
 
-  private apiUrl: string = "https://restcountries.eu/rest/v2";
-
+  private apiUrl: string = "http://api.countrylayer.com/v2";
+  private API_KEY: string = "?access_key=656b521b92d02d5944d87c2de895265f";
   constructor(private http: HttpClient) { }
 
   // esta es la llamada de la petición
   buscarPais( termino:string): Observable<Country[]>{
-    const url = `${this.apiUrl}/name/${termino}`;
+    const url = `${this.apiUrl}/name/${termino}${this.API_KEY}`;
     return this.http.get<Country[]>(url);
 
   }
