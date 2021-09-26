4a2d87a1fcd42f566f7d901e59022917d663052c
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-09-26 20:54:27 -0300

    var, injeccion del servicio y metodo buscar por capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
+  termino : string = '';
+  hayError: boolean = false;
+  paises  : Country[] = [];
 
-  ngOnInit(): void {
+  //  injectar la información de servicio pais nos permite utilizar
+  // los servicios definidos en PaisService
+  constructor(private paisService: PaisService) { }
+
+  // aqui es el consumo de nuestra peticion http.get que viene desde nuestro servicio
+  buscar(termino:string ) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital(this.termino)
+      .subscribe((paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },(err)=>{
+        this.hayError = true;
+        this.paises = [];
+      });
   }
 
+  
 }
