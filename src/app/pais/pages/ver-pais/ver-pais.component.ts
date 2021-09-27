90bd4acc88316370aa0978cf47797158a6c0d034
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-09-26 21:40:42 -0300

    inject router y  service, dos fomas de realizar sin rxjs

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute,
+               private paisService: PaisService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    // Forma 1 de realizar
+    // .subscribe(params =>{
+    //   console.log(params);
+    // forma 2 de realizar
+    .subscribe(({id}) =>{
+      console.log(id);
+      this.paisService.buscarPaisPorAlpha(id)
+      .subscribe(pais =>{
+        console.log(pais);
+      });
+    });
   }
 
 }
+// Vamos a crear otro ibservable esta vez en nuestro servicio.
